db76b547263e066fdba9ef397c61e51a2bc7e190
Morras321 <rascustom123@gmail.com>
2026-02-12 09:40:50 +0200

    PrintWorld function

--- app/src/main/java/com/GameWorld.java
+++ app/src/main/java/com/GameWorld.java
@@ -1,7 +1,7 @@
 package com;
 
 public class GameWorld {
-    
+
     // Properties
     private int worldHeight;
     private int worldWidth;
@@ -10,6 +10,7 @@ public class GameWorld {
     public void setWorldHeight(int worldHeight) {
         this.worldHeight = worldHeight;
     }
+
     public void setWorldWidth(int worldWidth) {
         this.worldWidth = worldWidth;
     }
@@ -17,9 +18,19 @@ public class GameWorld {
     public int getWorldHeight() {
         return worldHeight;
     }
+
     public int getWorldWidth() {
         return worldWidth;
     }
 
     // Methods
+    public void printWorld() {
+        for (int i = 0; i < this.worldHeight; i++) {
+            for (int j = 0; i < this.worldWidth; j++) {
+                // Print current coords, maybe use a function to determine if there is a wall or whatever, maybe Cell
+                //System.out.print();
+            }
+            System.out.println();
+        }
+    }
 }
