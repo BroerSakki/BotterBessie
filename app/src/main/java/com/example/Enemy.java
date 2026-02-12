f0c48049a39d042e6138cc01c660aaed6f8c712b
Morras321 <rascustom123@gmail.com>
2026-02-12 08:33:20 +0200

    Added enemy initial class

---
+++ app/src/main/java/com/example/Enemy.java
@@ -0,0 +1,19 @@
+
+public class Enemy {
+
+    public int x;
+    public int y;
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int beweeg() {
+        System.out.print("Walking");
+        return 0;
+    }
+}
