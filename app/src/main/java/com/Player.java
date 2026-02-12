13ae8023077ba94b4b54ecf2531894f9c48399e5
Snakyjean2 <piet42er@gmail.com>
2026-02-12 11:02:11 +0200

    v2

--- app/src/main/java/com/Player.java
+++ app/src/main/java/com/Player.java
@@ -1,60 +1,69 @@
 package com;
 
-public class Player {
+public class Player extends Entity {
 
-//     private String move;
-//     private int health ;
-//     private int stamina;
-//     private String dash;
+    private int health ;
+    private int stamina;
+    private String dash;
 
-//     public Player()
-//     {
-//         this("",0,0,"");
-//     }
+    public Player()
+    {
+        this(0,0,"");
+    }
 
-//     public Player(String move,int health,int stamina,String dash)
-//     {
-//         setMove(move);
-//     }
+    public Player(int health,int stamina,String dash)
+    {
+        setHealth(health);
+        setDash(dash);
+        setStamina(stamina);
+    }
 
-//     public void setMove(String move)
-//     {
-//         this.move = move;
-//     }
-
-//     public String getMove() 
-//     {
-//         return move;
-//     }
     
-//     public void setHealth(int health)
-//     {
-//         this.health = health;
-//     }
+    public void setHealth(int health)
+    {
+        this.health = health;
+    }
 
-//     public String getHealth() 
-//     {
-//         return health;
-//     }
+    public int getHealth() 
+    {
+        return health;
+    }
 
-//     public void setStamina(int stamina)
-//     {
-//         this.stamina = stamina;
-//     }
+    public void setStamina(int stamina)
+    {
+        this.stamina = stamina;
+    }
 
-//     public String getStamina() 
-//     {
-//         return stamina;
-//     }
+    public int getStamina() 
+    {
+        return stamina;
+    }
 
-//     public void setDash(String dash)
-//     {
-//         this.dash = dash;
-//     }
+    public void setDash(String dash)
+    {
+        this.dash = dash;
+    }
 
-//     public String getDash() 
-//     {
-//         return dash;
-//     }
+    public String getDash() 
+    {
+        return dash;
+    }
     
+    public void dash(int direction)
+    {
+        if (stamina >= 2)
+        {
+            stamina -= 2; // Consume stamina for dash
+            
+            // Move twice in the specified direction for dash effect
+            int[] newCoords = move(direction);
+            setX(newCoords[0]);
+            setY(newCoords[1]);
+            
+            // Move again for the dash distance
+            newCoords = move(direction);
+            setX(newCoords[0]);
+            setY(newCoords[1]);
+        }
+    }
  }
