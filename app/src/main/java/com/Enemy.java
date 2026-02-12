33d2530966a6bdaf8bcbf89d3563f796828e1323
BroerSakki <besterizak@gmail.com>
2026-02-12 15:01:57 +0200

    Reset Enemy class

--- app/src/main/java/com/Enemy.java
+++ app/src/main/java/com/Enemy.java
@@ -1,31 +1,5 @@
 package com;
 
-public class Enemy {
+public class Enemy extends Entity {
 
-    // Properties
-    public int x;
-    public int y;
-
-    // Accessors
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    // Methods
-    public int move(GameWorld world) {
-        System.out.print("Walking");
-        return 0;
-    }
 }
