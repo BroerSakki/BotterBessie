543890f7ed087c7fc9e1fd9f3915924032e630c4
BroerSakki <besterizak@gmail.com>
2026-02-12 15:02:18 +0200

    Entity - version 1.0

--- app/src/main/java/com/Entity.java
+++ app/src/main/java/com/Entity.java
@@ -2,50 +2,59 @@ package com;
 public class Entity {
     
     // Properties
-    public int x;
-    public int y;
+    public int row;
+    public int col;
 
     // Constructors
     public Entity() {
-
+        setRow(0);
+        setCol(0);
     }
 
     // Accessors
-    public int getX() {
-        return this.x;
+    public int getRow() {
+        return this.row;
     }
-    public int getY() {
-        return this.y;
+    public int getCol() {
+        return this.col;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public final void setRow(int row) {
+        this.row = row;
     }
-    public void setY(int y) {
-        this.y = y;
+    public final void setCol(int col) {
+        this.col = col;
     }
 
     // Methods
-    public int[] move(int direction) {
-        //Local Variables
-        int[] cordsCurrent = new int[]{getX(), getY()};
-        int[] cordsNew = cordsCurrent;
+    public boolean move(int direction) {
+        //Local variables
+        boolean validMove = false;
 
         switch (direction) {
-            case 1:
-                cordsNew[1]++;
-                break;
-            case 2:
-                cordsNew[0]++;
-                break;
-            case 3:
-                cordsNew[1]--;
-            case 4:
-                cordsNew[0]--;
-            default:
-                throw new Error("Invalid Direction");
+            case 1 -> {
+                if (row > 0) {
+                    this.row--;
+                    validMove = true;
+                }
+            }
+            case 2 -> {
+                this.col++;
+                validMove = true;
+            }
+            case 3 -> {
+                this.row++;
+                validMove = true;
+            }
+            case 4 -> {
+                if (col > 0) {
+                    this.col--;
+                    validMove = true;
+                }
+            }
+            default -> throw new IllegalArgumentException("Invalid direction: " + direction + ". Valid directions are 1-4.");
         }
 
-        return cordsNew;
+        return validMove;
     }
 }
