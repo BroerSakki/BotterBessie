23511dddc85c124b6b092780ebe93e6f548f0e65
BroerSakki <besterizak@gmail.com>
2026-02-13 11:35:49 +0200

    Make Path a child of Node

--- app/src/main/java/com/utils/Path.java
+++ app/src/main/java/com/utils/Path.java
@@ -1,38 +1,12 @@
 package com.utils;
 
-public class Path {
+public class Path extends Node {
     
-    // Properties
-    private char icon;
-    private String style;
-
     // Constructors
     public Path() {
-        this('_', Ansi.BG_GREEN);
+        this('_', Ansi.GREEN);
     }
     public Path(char icon, String style) {
-        setIcon(icon);
-        setStyle(style);
-    }
-
-    // Accessors
-    public final void setIcon(char icon) {
-        this.icon = icon;
-    }
-    public final void setStyle(String style) {
-        this.style = style;
-    }
-
-    public char getIcon() {
-        return icon;
-    }
-    public String getStyle() {
-        return style;
-    }
-
-    // Methods
-    @Override
-    public String toString() {
-        return Ansi.format(style, icon) + " ";
+        super(icon, style);
     }
 }
