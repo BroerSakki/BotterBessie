b91a32225e983b537677418767f916648d3ea695
BroerSakki <besterizak@gmail.com>
2026-02-18 13:07:23 +0200

    Enemy class versio 1.0

--- app/src/main/java/com/Enemy.java
+++ app/src/main/java/com/Enemy.java
@@ -1,5 +1,14 @@
 package com;
 
+import com.utils.Ansi;
+
 public class Enemy extends Entity {
 
+    // Constructors
+    public Enemy() {
+        this('!', Ansi.RED);
+    }
+    public Enemy(char icon, String style) {
+        super(icon, style);
+    }
 }
